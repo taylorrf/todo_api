ad7578047de84f0e644b0ab5fe815552104767e5
Tailor Fontela <taylorrf@gmail.com>
2016-10-21 15:35:15 -0200

    providing a description of user key not found on authentication

--- routes/users.js
+++ routes/users.js
@@ -19,6 +19,14 @@ var users = function(app){
   *       "firebase_key": "123123123"
   *     }
   *
+  * @apiError UserNotFound The <code>firebase_key</code> of the User was not found.
+  * @apiErrorExample {json} Error-Response:
+  *     HTTP/1.1 403 Forbidden
+  *     {
+  *       "success": false,
+  *       "message": "No User Key provided."
+  *     }
+  *
   */
 
   /**
