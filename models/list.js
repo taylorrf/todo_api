45db1e47745a002a549140d5f907147880cd7fbc
Tailor Fontela <taylorrf@gmail.com>
2016-10-23 17:10:53 -0200

    readme aditional infos

--- models/list.js
+++ models/list.js
@@ -18,3 +18,30 @@ module.exports = function(sequelize, DataTypes) {
   });
   return List;
 };
+
+
+/*
+
+User {
+  id: firebase_key
+  name: "John Armless"
+  dreams: [array of dreams]
+}
+
+Dream {
+  id: 1 // long
+  category: "Travel" // any string
+  subcategory: "Camping" // any string
+  user: {User entity}
+  layers: [array of layers]
+}
+
+Layer {
+  id: 1 // long
+  type: // "photo", "video", "product" (constant string)
+  description: "This is my dream" // the description of this layer
+  url: "http://url.to.resource"
+  productId: // Only for shopify's product ID (https://help.shopify.com/api/reference/product)
+}
+
+*/
